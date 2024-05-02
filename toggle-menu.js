dd723ca995de77bafa1aadcd8802e60bc670badc
TechiesKE <okothgerald449@gmail.com>
2024-05-02 09:28:29 +0300

    Add .js files

---
+++ toggle-menu.js
@@ -0,0 +1,11 @@
+	// Function to toggle the menu open and close
+	function toggleMenu() {
+		var menu = document.getElementById("menuLinks");
+
+		if (menu.style.display === "block") {
+			menu.style.display = "none";
+			
+		} else {
+			menu.style.display = "block";
+		}
+	}
