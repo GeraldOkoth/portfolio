ca73c8a2904a022b6c9b9c2da74665529b1768f5
Gerald Okoth <okothgerald449@gmail.com>
2024-05-18 10:59:56 +0300

    Resolve toggle menu error

--- toggle-menu.js
+++ toggle-menu.js
@@ -4,12 +4,16 @@
 
 		if (menu.style.display === "block") {
 			menu.style.display = "none";
+			document.getElementById("hamburgerOpen").style.visibility = "visible";
+			document.getElementById("hamburgerClose").style.visibility = "hidden";
 			
 		} else {
 			menu.style.display = "block";
+			document.getElementById("hamburgerClose").style.display = "block";
+			document.getElementById("hamburgerOpen").style.visibility = "hidden";
 		}
 	}
 
-	function changeTheme(){
-		javascript: void(document.body.style.backgroundColor = 'red')
-	}
+	// function changeTheme(){
+	// 	javascript: void(document.body.style.backgroundColor = 'red')
+	// }
