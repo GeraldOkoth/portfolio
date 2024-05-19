eb66a897435e7572af930e68cc676906cd23a8f7
Gerald Okoth <okothgerald449@gmail.com>
2024-05-19 17:37:21 +0300

    Style the navbar on small screen

--- script.js
+++ script.js
@@ -10,3 +10,18 @@
 // function hideText(){
 //     document.getElementById('demo').style.visibility = "hidden";
 // }
+document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.getElementById('menuLinks');
+    const links = nav.getElementsByTagName('a');
+    
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', function() {
+        // Remove the active class from all links
+        for (let j = 0; j < links.length; j++) {
+          links[j].classList.remove('active');
+        }
+        // Add the active class to the clicked link
+        this.classList.add('active');
+      });
+    }
+  });
