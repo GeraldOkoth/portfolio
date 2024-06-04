5fd8d63755c38e5916c62daa7c852389f3670d8b
Gerald Okoth <okothgerald449@gmail.com>
2024-06-04 10:36:15 +0300

    Create new folders

---
+++ Js/send-email.js
@@ -0,0 +1,12 @@
+//Function to send email.
+function sendMail(){
+    let params = {
+        firstname : document.getElementById("firstname").value,
+        lastname : document.getElementById("lastname").value,
+        email : document.getElementById("email").value,
+        phoneNumber : document.getElementById("phoneNumber").value,
+        mesgSubject : document.getElementById("mesgSubject").value,
+        msg : document.getElementById("msg").value
+    }
+    emailjs.send("service_lxmqzq5", "template_6zufajn", params).then(alert("Email has been sent successfully! We will respond to you shortly."))
+}
