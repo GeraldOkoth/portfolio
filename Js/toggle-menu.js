059ea0e4e4736dde090e5cef005ccc511315cf3d
Gerald Okoth <okothgerald449@gmail.com>
2024-06-24 16:40:13 +0300

    Designed toggle menu

--- Js/toggle-menu.js
+++ Js/toggle-menu.js
@@ -1,18 +1,18 @@
 // Function to toggle the menu open and close
-function toggleMenu() {
-    var menu = document.getElementById("menuLinks");
-    var hamburgerOpen = document.getElementById("hamburgerOpen");
-    var hamburgerClose = document.getElementById("hamburgerClose");
+let menu = document.getElementById("menuLinks");
+let hamburgerOpen = document.getElementById("hamburgerOpen");
+let hamburgerClose = document.getElementById("hamburgerClose");
 
+function toggleMenu() {
     if (menu.classList.contains("show")) {
         menu.classList.remove("show");
         menu.classList.add("hide");
-        setTimeout(() => { menu.style.display = "none"; }, 300); // Match the transition duration
+        setTimeout(() => { menu.style.height = "0"; }, 300); // Match the transition duration
         hamburgerOpen.style.visibility = "visible";
         hamburgerClose.style.visibility = "hidden";
     } else {
         menu.classList.remove("hide");
-        menu.style.display = "block"; // Ensure display is set before adding the show class
+        menu.style.height = "100vh"; // Ensure display is set before adding the show class
         setTimeout(() => { menu.classList.add("show"); }, 10); // Small delay to trigger the transition
         hamburgerOpen.style.visibility = "hidden";
         hamburgerClose.style.visibility = "visible";
