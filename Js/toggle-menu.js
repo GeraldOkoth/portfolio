1c74d2a8e17197f05efc09bf0ed51b94b5362766
Gerald Okoth <okothgerald449@gmail.com>
2024-06-24 17:51:47 +0300

    Refactor toggle menu

--- Js/toggle-menu.js
+++ Js/toggle-menu.js
@@ -7,15 +7,15 @@ function toggleMenu() {
     if (menu.classList.contains("show")) {
         menu.classList.remove("show");
         menu.classList.add("hide");
-        setTimeout(() => { menu.style.height = "0"; }, 300); // Match the transition duration
+        setTimeout(() => { menu.style.width = "0px"; }, 300); // Match the transition duration
         hamburgerOpen.style.visibility = "visible";
-        hamburgerClose.style.visibility = "hidden";
+        // hamburgerClose.style.visibility = "hidden";
     } else {
         menu.classList.remove("hide");
-        menu.style.height = "100vh"; // Ensure display is set before adding the show class
+        menu.style.width = "250px"; // Ensure display is set before adding the show class
         setTimeout(() => { menu.classList.add("show"); }, 10); // Small delay to trigger the transition
         hamburgerOpen.style.visibility = "hidden";
-        hamburgerClose.style.visibility = "visible";
+        // hamburgerClose.style.visibility = "visible";
     }
 }	
 // function changeTheme(){
