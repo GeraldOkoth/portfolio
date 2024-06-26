a9f39792a7e96834920d219cda82aaf84949df09
Gerald Okoth <okothgerald449@gmail.com>
2024-06-26 10:56:35 +0300

    Add Typescript file

--- Js/toggle-menu.js
+++ Js/toggle-menu.js
@@ -1,7 +1,7 @@
 // Function to toggle the menu open and close
 let menu = document.getElementById("menuLinks");
 let hamburgerOpen = document.getElementById("hamburgerOpen");
-// let hamburgerClose = document.getElementById("hamburgerClose");
+let hamburgerClose = document.getElementById("hamburgerClose");
 
 function toggleMenu() {
     if (menu.classList.contains("show")) {
@@ -9,15 +9,12 @@ function toggleMenu() {
         menu.classList.add("hide");
         setTimeout(() => { menu.style.width = "0px"; }, 300); // Match the transition duration
         hamburgerOpen.style.visibility = "visible";
-        // hamburgerClose.style.visibility = "hidden";
+        hamburgerClose.style.visibility = "hidden";
     } else {
         menu.classList.remove("hide");
         menu.style.width = "250px"; // Ensure display is set before adding the show class
         setTimeout(() => { menu.classList.add("show"); }, 10); // Small delay to trigger the transition
         hamburgerOpen.style.visibility = "hidden";
-        // hamburgerClose.style.visibility = "visible";
+        hamburgerClose.style.visibility = "visible";
     }
-}	
-// function changeTheme(){
-// 	javascript: void(document.body.style.backgroundColor = 'red')
-// }
+}
