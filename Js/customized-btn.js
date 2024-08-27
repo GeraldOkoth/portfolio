13f749dc5db6a7f78bb7f13a7c62d187cf4bc94e
Gerald Okoth <okothgerald449@gmail.com>
2024-08-27 13:08:34 +0300

    Add more customization

--- Js/customized-btn.js
+++ Js/customized-btn.js
@@ -1,5 +1,6 @@
 // This code asks the webpage visitor to enter name upon clicking the Resume download button on the home page
 const downloadCvBtn = document.getElementById("DownloadBtn");
+const myHref = downloadCvBtn.getAttribute("href");
 
 function displayAlert() {
     const visitorName = prompt("Please enter your name on this prompt:");
@@ -7,8 +8,16 @@ function displayAlert() {
     displayVisitorName = localStorage.getItem("name")
     if (!displayVisitorName)
         displayAlert();
-    else
-        alert("Sorry, " +visitorName + " Resume has not yet been uploaded, Please check again later.");
+    else {
+        if (myHref === "../Assets/gerald_okoth_myjobmag_curriculum_vitae.pdf") {
+            alert("Thank you " +visitorName + " for your download, Resume will be ready in ashort while.");
+        }
+        else {
+            alert("Sorry, " +visitorName + " Resume has not yet been uploaded, Please check again later.");
+        }
+
+    }
 }
 
 downloadCvBtn.addEventListener("click", displayAlert); 
+
