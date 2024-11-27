f993b5ea78592b301f0d2c866533edbce4360ce0
Gerald Okoth <okothgerald449@gmail.com>
2024-11-27 22:45:43 +0300

    Create react components

---
+++ portfolio/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import HomePage from './HomePage';
+import Slideshow from './Slideshow';
+import AboutPage from './AboutPage';
+import Services from './Services';
+import Testimonials from './Testimonials';
+import Faq from './Faq';
+import Contact from './Contact';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      <Slideshow />
+      <AboutPage />
+      <Services />
+      <Testimonials />
+      <Faq />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
