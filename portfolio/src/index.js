7304d0c91c8e576b7cfb771fea0fc41c8819d028
Gerald Okoth <okothgerald449@gmail.com>
2025-01-24 09:22:07 +0300

    correct code

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
