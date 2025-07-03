b8a8cc99da89cebc3c24eff280f167db9ff39b6d
GeraldOKoth <okothgerald449@gmail.com>
2025-07-03 16:20:21 +0300

    Add framer motion

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import textAnimate from "./text-animation";
 import TopNavigationBar from "../../components/header/TopNavigationBar";
 import { easeInOut, generateLinearEasing, motion } from "framer-motion";
