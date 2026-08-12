0919a5c3bc17039deff43ba6e9a7a392e8f57a1f
Gerald Okoth <okothgerald449@gmail.com>
2026-08-12 11:58:44 +0300

    refactor: replace footer fontawesome icons with react icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,68 +1,67 @@
 import React from "react";
 import { easeInOut, motion } from "framer-motion";
-
+import { FaLinkedin, FaGithub, FaYoutube, FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
 
 function Footer() {
-
   const socialMediaLinks = [
     {
       href: "https://www.linkedin.com/in/geraldokoth/",
-      iconClass: "fa-brands fa-linkedin",
-      label: "LinkedIn"
+      label: "LinkedIn",
+      icon: <FaLinkedin />,
     },
     {
       href: "https://github.com/GeraldOkoth",
-      iconClass: "fa-brands fa-github",
-      label: "GitHub"
+      abel: "GitHub",
+      icon: <FaGithub />,
     },
     {
       href: "https://www.youtube.com/@geraldokothKE",
-      iconClass: "fa-brands fa-youtube",
-      label: "YouTube"
+      label: "YouTube",
+      icon: <FaYoutube />,
     },
     {
       href: "https://x.com/gerald_okothKE",
-      iconClass: "fa-brands fa-twitter",
-      label: "Twitter"
+      label: "Twitter",
+      icon: <FaTwitter />,
     },
     {
       href: "https://www.instagram.com/okothgerald449",
-      iconClass: "fa-brands fa-instagram",
-      label: "Instagram"
+      label: "Instagram",
+      icon: <FaInstagram />,
     },
     {
       href: "https://www.facebook.com/gerald.okoth.944/",
-      iconClass: "fa-brands fa-facebook",
-      label: "Facebook"
-    }
-  ]
+      label: "Facebook",
+      icon: <FaFacebook />,
+    },
+  ];
 
   return (
     <section className="footer">
-        <hr />
-        <motion.div
-          initial={{ opacity: 0, y: -30 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ transitionTimingFunction: easeInOut, delay: .4 }}
-          className="footer-content"
-        >
-          <p className="copyright">
-            &copy; {new Date().getFullYear()}, TechiesKE. All rights reserved.
-          </p>
-          <div className="social-media-icons">
-            {socialMediaLinks.map((link, index) => (
-              <a
-                key={index}
-                href={link.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                title={link.label}
-              >
-                <i className={link.iconClass}></i>
-              </a>
-            ))}
-          </div>
-        </motion.div>
+      <hr />
+      <motion.div
+        initial={{ opacity: 0, y: -30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ transitionTimingFunction: easeInOut, delay: 0.4 }}
+        className="footer-content"
+      >
+        <p className="copyright">
+          &copy; {new Date().getFullYear()} TechiesKE. All rights reserved.
+        </p>
+        <div className="social-media-icons">
+          {socialMediaLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              title={link.label}
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+      </motion.div>
     </section>
   );
 }
